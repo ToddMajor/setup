2550dea8fb7a36693ff119a72e2ca15e45035099
ToddMajor <majortodd1@gmail.com>
2023-05-02 22:10:43 -0500

    Update ruby_setup.sh

--- ruby/ruby_setup.sh
+++ ruby/ruby_setup.sh
@@ -1,1 +1,12 @@
 #! /bin/sh
+
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+
+echo 'eval "$(~/.rbenv/bin/rbenv init - bash)"' >> ~/.bashrc
+
+# close terminal
+
+sudo apt-get install autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+
+rbenv install 3.2.2 # or whatever version is newest and stable
+rbenv global 3.2.2 #same version as above
