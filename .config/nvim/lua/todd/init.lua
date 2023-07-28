6383a24e1d913ac5b7ea1cf0a8207211e628e045
Todd <todd.major@echo.com>
2023-07-28 14:34:29 -0500

    adding first setup of nvim

---
+++ .config/nvim/lua/todd/init.lua
@@ -0,0 +1,2 @@
+require("todd.remap")
+require("todd.set")
---
+++ .config/nvim/lua/todd/set.lua
@@ -0,0 +1,15 @@
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+
+vim.opt.termguicolors = true
+
