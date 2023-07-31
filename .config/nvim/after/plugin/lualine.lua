5a145c4bbe63af0e123e90a05126a87de5a6d355
ToddMajor <majortodd1@gmail.com>
2023-07-31 11:15:13 -0500

    Create and add lualine.lua config

---
+++ .config/nvim/after/plugin/lualine.lua
@@ -0,0 +1,8 @@
+require('lualine').setup{
+    options = {
+        icons_enabled = false,
+        theme = 'duskfox',
+        component_separators = '|',
+        section_separators = '',
+      }
+}
